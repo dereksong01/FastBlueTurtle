fc1d7e126a59d7a5ae438bf1597fde4c74aa9a92
Alvin Ye <aye4@stuy.edu>
2018-05-10 13:59:14 -0400

    updated APQ

--- v2/ArrayPriorityQueue.java
+++ v2/ArrayPriorityQueue.java
@@ -6,123 +6,121 @@
 import java.util.ArrayList;
 
 /*======================
-In this implementation, the front of the array is the front of the queue and the
-back of the array is the back of the queue.
-=======================*/
+  In this implementation, the front of the array is the front of the queue and the
+  back of the array is the back of the queue.
+  =======================*/
 
 
 public class ArrayPriorityQueue implements PriorityQueue{
 
-	private ArrayList<String> queue;
-	private int size;
+    private ArrayList<String> queue;
 
-	public ArrayPriorityQueue(){
-		queue = new ArrayList<String>();
-		size = 0;
+    public ArrayPriorityQueue(){
+	queue = new ArrayList<String>();
+	size = 0;
+    }
+
+    //enqueues value to the end of the array (back of queue)
+    //O(1) 
+    public void add(String x){ 
+	queue.add(x);
+    }
+
+    //O(1)
+    public boolean isEmpty(){
+	return size == 0;
+    }
+
+    //O(1)
+    public int size(){
+	return queue.size();
+    }
+
+    //returns the smallest value in the queue
+    //O(n) 
+    public String peekMin(){
+	if(isEmpty()){
+	    throw new RuntimeException();
 	}
-
-	//enqueues value to the end of the array (back of queue)
-	//O(1) 
-	public void add(String x){ 
-		queue.add(x);
-		size++;
+		
+	String retStr = queue.get(0);
+		
+	for(String s: queue){
+	    if(s.compareTo(retStr) < 0){
+		retStr = s;
+	    }
 	}
+	return retStr;
+    }
 
-	//O(1)
-	public boolean isEmpty(){
-		return size == 0;
+    //removes the smallest value in the queue and returns the removed value
+    //O(n)
+    public String removeMin(){
+
+	if(isEmpty()){
+	    throw new RuntimeException();
 	}
+		
+	String retStr = queue.get(0);
+	int indexToRemove = 0;
 
-	//O(1)
-	public int size(){
-		return size;
+	for(int i = 0; i < queue.size(); i++){
+	    if(queue.get(i).compareTo(retStr) < 0 ){
+		retStr = queue.get(i);
+		indexToRemove = i;
+	    }
 	}
+	queue.remove(indexToRemove);
+	return retStr;
+    }
 
-	//returns the smallest value in the queue
-	//O(n) 
-	public String peekMin(){
-		String retStr = queue.get(0);
-
-		if(isEmpty()){
-			throw new RuntimeException();
-		}
-
-		for(String s: queue){
-			if(s.compareTo(retStr) < 0){
-				retStr = s;
-			}
-		}
-		return retStr;
+    //O(n)
+    public String toString(){
+	String retStr = "[";
+	for(String s: queue){
+	    retStr += (s + ",");
 	}
-
-	//removes the smallest value in the queue and returns the removed value
-	//O(n)
-	public String removeMin(){
-		String retStr = queue.get(0);
-		int indexToRemove = 0;
-
-		if(isEmpty()){
-			throw new RuntimeException();
-		}
-
-		for(int i = 0; i < queue.size(); i++){
-			if(queue.get(i).compareTo(retStr) < 0 ){
-				retStr = queue.get(i);
-				indexToRemove = i;
-			}
-		}
-		queue.remove(indexToRemove);
-		size--;
-		return retStr;
-	}
-
-	//O(n)
-	public String toString(){
-		String retStr = "[";
-		for(String s: queue){
-			retStr += (s + ",");
-		}
-		return retStr.substring(0, retStr.length() - 1) + "]";
-	}
+	return retStr.substring(0, retStr.length() - 1) + "]";
+    }
 
 
-	public static void main(String[] args){
-		ArrayPriorityQueue q = new ArrayPriorityQueue();
+    public static void main(String[] args){
+	ArrayPriorityQueue q = new ArrayPriorityQueue();
 
-		System.out.println(q.isEmpty());
+	System.out.println(q.isEmpty());
 
-		q.add("b");
-		q.add("a");
-		q.add("d");
-		q.add("c");
-		q.add("z");
-		q.add("w");
-		q.add("l");
+	q.add("b");
+	q.add("a");
+	q.add("d");
+	q.add("c");
+	q.add("z");
+	q.add("w");
+	q.add("l");
 
-		System.out.println(q);
-		System.out.println("size: " + q.size());
+	System.out.println(q);
+	System.out.println("size: " + q.size());
 
 
-		System.out.print("Removing...");
-		System.out.println(q.removeMin());
-		System.out.println(q);
+	System.out.print("Removing...");
+	System.out.println(q.removeMin());
+	System.out.println(q);
 
-		System.out.print("Removing...");
-		System.out.println(q.removeMin());
-		System.out.println(q);
+	System.out.print("Removing...");
+	System.out.println(q.removeMin());
+	System.out.println(q);
 
-		System.out.print("Removing...");
-		System.out.println(q.removeMin());
-		System.out.println(q);
+	System.out.print("Removing...");
+	System.out.println(q.removeMin());
+	System.out.println(q);
 
-		System.out.print("Removing...");
-		System.out.println(q.removeMin());
-		System.out.println(q);
+	System.out.print("Removing...");
+	System.out.println(q.removeMin());
+	System.out.println(q);
 
-		System.out.print("Removing...");
-		System.out.println(q.removeMin());
-		System.out.println(q);		
-		System.out.println("size: " + q.size());
-	}
+	System.out.print("Removing...");
+	System.out.println(q.removeMin());
+	System.out.println(q);		
+	System.out.println("size: " + q.size());
+    }
 
 }//end class
